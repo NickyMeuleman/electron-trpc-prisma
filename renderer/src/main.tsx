b9198137aa52baef66591b342f75bc7e741fabcd
Nicky Meuleman <nicky.dev@outlook.com>
2022-09-24 21:57:12 +0200

    ensure root exists before rendering

--- renderer/src/main.tsx
+++ renderer/src/main.tsx
@@ -3,8 +3,12 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const rootEl = document.getElementById("root");
+
+if (rootEl) {
+  ReactDOM.createRoot(rootEl).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+}
