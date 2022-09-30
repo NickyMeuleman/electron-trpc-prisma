c75e16077dd65c16a92e1f4df0efde001e45e31c
Nicky Meuleman <nicky.dev@outlook.com>
2022-09-30 15:13:46 +0200

    seperate linting for react renderer

--- renderer/.eslintrc.cjs
+++ renderer/.eslintrc.cjs
@@ -2,22 +2,37 @@
 
 /** @type {import("eslint").Linter.Config} */
 const eslintConfig = {
-  root: true,
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    tsconfigRootDir: __dirname,
-    project: ["./*/tsconfig.json", "./tsconfig.node.json", "./tsconfig.json"],
+  root: false,
+  env: {
+    browser: true,
+    es2021: true,
   },
-  plugins: ["@typescript-eslint"],
   extends: [
     "eslint:recommended",
     "plugin:@typescript-eslint/eslint-recommended", // disable core eslint rules that conflict with replacement @typescript-eslint rules
     "plugin:@typescript-eslint/recommended",
     "plugin:@typescript-eslint/recommended-requiring-type-checking",
     "plugin:@typescript-eslint/strict",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:react-hooks/recommended",
     "prettier", // config-prettier disables eslint rules that conflict with prettier
   ],
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    tsconfigRootDir: __dirname,
+    ecmaVersion: "latest",
+    sourceType: "module",
+    project: ["./tsconfig.json", "../tsconfig.node.json"],
+  },
+  plugins: ["react", "react-hooks", "@typescript-eslint"],
   rules: {},
+  overrides: [],
 };
 
 module.exports = eslintConfig;
--- renderer/src/Home.tsx
+++ renderer/src/Home.tsx
@@ -20,10 +20,10 @@ function Home() {
   return (
     <div className="App">
       <div>
-        <a href="https://vitejs.dev" target="_blank">
+        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
           <img src="vite.svg" className="logo" alt="Vite logo" />
         </a>
-        <a href="https://reactjs.org" target="_blank">
+        <a href="https://reactjs.org" target="_blank" rel="noreferrer">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
